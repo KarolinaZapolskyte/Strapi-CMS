58a7c9c263ac04ecbac8b1dc2be1266ca9eacad9
KarolinaZapolskyte <43141874+KarolinaZapolskyte@users.noreply.github.com>
2021-05-05 10:46:52 +0200

    initial strapi app create

---
+++ strapi-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0ee186a591d39ea41eaba389e6fdc2dd'),
+    },
+  },
+});
---
+++ strapi-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3a313a6e-d761-4ed6-a219-7f14a45e27d8'
+};
